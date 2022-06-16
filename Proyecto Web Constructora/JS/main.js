eb962631f61d402b1ea12aaed88d9cf778f28a07
Yamil <yamilsaad00@gmail.com>
2022-06-15 23:23:35 -0300

    js efect

--- Proyecto Web Constructora/JS/main.js
+++ Proyecto Web Constructora/JS/main.js
@@ -12,3 +12,33 @@ window.onscroll=()=>{
         document.getElementById("btnScrolltop").style.display="none";
     }
 }
+
+window.addEventListener('scroll', function()  {
+    let elements = document.getElementsByClassName('mision-img');
+    let screenSize = window.innerHeight;
+    
+      for(var i = 0; i < elements.length; i++) {
+        let element = elements[i];
+  
+        if(element.getBoundingClientRect().top < screenSize) {
+          element.classList.add('visible');
+        } else {
+          element.classList.remove('visible');
+        }
+      }
+  });
+
+  window.addEventListener('scroll', function()  {
+    let elements = document.getElementsByClassName('animacion');
+    let screenSize = window.innerHeight;
+    
+      for(var i = 0; i < elements.length; i++) {
+        let element = elements[i];
+  
+        if(element.getBoundingClientRect().top < screenSize) {
+          element.classList.add('visible');
+        } else {
+          element.classList.remove('visible');
+        }
+      }
+  });
